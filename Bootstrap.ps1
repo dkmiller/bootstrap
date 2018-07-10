f02a78d0c1e3e10792b99976702f9d6aa4e2ac17
Daniel Miller <daniel.keegan.miller@gmail.com>
2018-07-10 14:44:14 -0700

    Update Bootstrap.ps1

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -12,18 +12,18 @@ $ErrorActionPreference = 'Stop'
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # See https://chocolatey.org/packages/<package name> for more details on any of these.
-choco install --yes anaconda3 --params '/InstallationType:AllUsers /AddToPath:1 /RegisterPython:1'
-choco install --yes googlechrome
-choco install --yes docker-for-windows
-choco install --yes fiddler
-choco install --yes git.install --params '/SChannel'
-choco install --yes ilspy
-choco install --yes jdk8
-choco install --yes linqpad
-choco install --yes office365proplus
-choco install --yes dotnetcore-sdk
-choco install --yes netfx-4.7.1-devpack
-choco install --yes r.project
-choco install --yes r.studio
-choco install --yes vscode --params '/NoDesktopIcon /NoQuickLaunchIcon'
-choco install --yes visualstudio2017enterprise
+choco install --yes --fail-on-standard-error anaconda3 --params '/InstallationType:AllUsers /AddToPath:1 /RegisterPython:1'
+choco install --yes --fail-on-standard-error googlechrome
+choco install --yes --fail-on-standard-error docker-for-windows
+choco install --yes --fail-on-standard-error fiddler
+choco install --yes --fail-on-standard-error git.install --params '/SChannel'
+choco install --yes --fail-on-standard-error ilspy
+choco install --yes --fail-on-standard-error jdk8
+choco install --yes --fail-on-standard-error linqpad
+choco install --yes --fail-on-standard-error office365proplus
+choco install --yes --fail-on-standard-error dotnetcore-sdk
+choco install --yes --fail-on-standard-error netfx-4.7.1-devpack
+choco install --yes --fail-on-standard-error r.project
+choco install --yes --fail-on-standard-error r.studio
+choco install --yes --fail-on-standard-error vscode --params '/NoDesktopIcon /NoQuickLaunchIcon'
+choco install --yes --fail-on-standard-error visualstudio2017enterprise
