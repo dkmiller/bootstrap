770d199c9d5d851e108a12b53a49d09d11d62443
Daniel Miller <daniel.keegan.miller@gmail.com>
2018-12-27 08:12:37 -0800

    Update Docker package, de-duplicate and fix order of packages.

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -35,16 +35,13 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 # See https://chocolatey.org/packages/<package name> for more details on any of these.
 @(
     'adobereader',
-    'azure-cli',
     'docfx',
-    'docker-for-windows',
+    'docker-desktop',
     'dotnetcore-sdk',
     'fiddler',
-    'googlechrome',
-    'fiddler',
     'git',
+    'googlechrome',
     'ilspy',
-    'intellijidea-community',
     'jdk8',
     'linqpad',
     'microsoft-teams',
