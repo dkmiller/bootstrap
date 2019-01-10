520b02d6d7b65bcffb18e77cd6f62ba933d4fd26
Daniel Miller <daniel.keegan.miller@gmail.com>
2019-01-10 08:10:24 -0800

    Also install Fira Code
    Fira code gives beautiful programming ligatures ( https://github.com/tonsky/FiraCode ).

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -39,6 +39,7 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
     'docker-desktop',
     'dotnetcore-sdk',
     'fiddler',
+    'firacode',
     'git',
     'googlechrome',
     'ilspy',
