6931878a1bb93c26f4c3a760aec4704245871a3b
Dan Miller <danmill@microsoft.com>
2021-07-22 09:47:21 -0700

    make prompt git-aware

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,47 @@
+# https://stackoverflow.com/a/44411205
+function Get-BranchName {
+    # https://en.wikipedia.org/wiki/ANSI_escape_code
+    $Blue = "`e[94m"
+    $Red = "`e[31m"
+    $Reset = "`e[0m"
+    $Yellow = "`e[93m"
+
+    try {
+        $Branch = git rev-parse --abbrev-ref HEAD
+
+        if ($Branch -eq "HEAD") {
+            # We're probably in detached HEAD state, so print the SHA
+            $branch = git rev-parse --short HEAD
+
+            # https://superuser.com/a/1259916
+            $AnnotatedBranch = "$Red$Branch$Reset"
+        }
+        else {
+            # We're on an actual branch, so print it
+            $AnnotatedBranch = "$Blue$Branch$Reset"
+        }
+    }
+    catch {
+        # We'll end up here if we're in a newly initiated git repo
+        $AnnotatedBranch = "$($Yellow)N/A$($Reset)"
+    }
+
+    return " ($AnnotatedBranch)"
+}
+
+
 # https://superuser.com/a/446836
 function prompt {
     $p = Split-Path -leaf -path (Get-Location)
-    "$p\ > "
+
+    if (Test-Path .git) {
+        $Branch = Get-BranchName
+        $p += $Branch
+    }
+
+    $p += "\ > "
+
+    return $p
 }
 
 function bootstrap {
