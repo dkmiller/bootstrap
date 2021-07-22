c5e7dedf63805f20827b5c1ca3e3dbaaca459e7d
Dan Miller <danmill@microsoft.com>
2021-07-22 08:55:32 -0700

    add boostrap and clear-gitrepo functions to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,21 @@ function prompt {
     $p = Split-Path -leaf -path (Get-Location)
     "$p\ > "
 }
+
+function bootstrap {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-WebRequest https://raw.githubusercontent.com/dkmiller/bootstrap/vnext/Bootstrap.ps1 | Invoke-Expression
+}
+
+function Clear-GitRepo {
+    $ErrorActionPreference = "Stop"
+
+    # https://stackoverflow.com/a/44750379
+    $DefaultBranch = git symbolic-ref refs/remotes/origin/HEAD | ForEach-Object { $_.Split("/")[-1] }
+    Write-Warning "Clearning repo assuming default branch $DefaultBranch"
+
+    git checkout $DefaultBranch
+    git pull
+    git clean -xdf
+    git remote prune origin
+}
