23656dedf89892702327f4938a5728b10396ace0
Daniel Miller <daniel.keegan.miller@gmail.com>
2021-11-17 19:06:26 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,10 @@ cd ~
 az keyvault secret show --id https://danmill-kv.vault.azure.net/secrets/id-rsa-pub --query value -o tsv > ~/.ssh/id_rsa.pub
 az keyvault secret show --id https://danmill-kv.vault.azure.net/secrets/id-rsa --query value -o tsv > ~/.ssh/id_rsa
 
+# https://www.techrepublic.com/article/how-to-easily-add-an-ssh-fingerprint-to-your-knownhosts-file-in-linux/
+ssh-keyscan -H vs-ssh.visualstudio.com >> ~/.ssh/known_hosts
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
 git clone msdata@vs-ssh.visualstudio.com:v3/msdata/Vienna/aml-ds
 git clone msdata@vs-ssh.visualstudio.com:v3/msdata/Vienna/aml-ds-internal
 
