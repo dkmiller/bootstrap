83b73ccb5d5a65ea2f8d3d32d9d00e5d97811534
Daniel Miller <daniel.keegan.miller@gmail.com>
2021-11-17 18:52:14 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 # Run this using:
 # curl https://raw.githubusercontent.com/dkmiller/bootstrap/ubuntu/bootstrap.sh | sudo bash
 
-if ! az account show; then
+# https://stackoverflow.com/a/26675771
+if ! az account list; then
   az login
 fi
 
 cd ~
+
+az keyvault secret show --id https://danmill-kv.vault.azure.net/secrets/id-rsa-pub --query value -o tsv > ~/.ssh/id_rsa.pub
+az keyvault secret show --id https://danmill-kv.vault.azure.net/secrets/id-rsa --query value -o tsv > ~/.ssh/id_rsa
