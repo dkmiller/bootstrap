af4b3055390c0558022ca8e776f076d0a2ba05f6
Daniel Miller <daniel.keegan.miller@gmail.com>
2021-11-17 18:54:13 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,9 @@ cd ~
 
 az keyvault secret show --id https://danmill-kv.vault.azure.net/secrets/id-rsa-pub --query value -o tsv > ~/.ssh/id_rsa.pub
 az keyvault secret show --id https://danmill-kv.vault.azure.net/secrets/id-rsa --query value -o tsv > ~/.ssh/id_rsa
+
+git clone msdata@vs-ssh.visualstudio.com:v3/msdata/Vienna/aml-ds
+git clone msdata@vs-ssh.visualstudio.com:v3/msdata/Vienna/aml-ds-internal
+
+git clone git@github.com:dkmiller/odin.git
+git clone git@github.com:dkmiller/tidbits.git
